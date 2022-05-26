2c99b8ffc5b737d251225b920caf319ebbe45dcc
mustaphazahdour <zahdourmoustafa@gmail.com>
2022-05-26 08:57:12 -0700

    modification

--- drivers_app/lib/assistants/assistant_methods.dart
+++ drivers_app/lib/assistants/assistant_methods.dart
@@ -7,6 +7,7 @@ import 'package:drivers_app/models/directions.dart';
 import 'package:drivers_app/models/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_geofire/flutter_geofire.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
@@ -75,5 +76,49 @@ class AssistantMethods
     directionDetailsInfo.duration_value = responseDirectionApi["routes"][0]["legs"][0]["duration"]["value"];
 
     return directionDetailsInfo;
+  }
+
+  static pauseLiveLocationUpdates()
+  {
+    streamSubscriptionPosition!.pause();
+    Geofire.removeLocation(currentFirebaseUser!.uid);
+  }
+
+  static resumeLiveLocationUpdates()
+  {
+    streamSubscriptionPosition!.resume();
+    Geofire.setLocation(
+        currentFirebaseUser!.uid,
+        driverCurrentPosition!.latitude,
+        driverCurrentPosition!.longitude
+    );
+  }
+
+  static double calculateFareAmountFromOriginToDestination(DirectionDetailsInfo directionDetailsInfo)
+  {
+    double timeTraveledFareAmountPerMinute = (directionDetailsInfo.duration_value! / 60) * 0.1;
+    double distanceTraveledFareAmountPerKilometer = (directionDetailsInfo.duration_value! / 1000) * 0.1;
+
+    //USD
+    double totalFareAmount = timeTraveledFareAmountPerMinute + distanceTraveledFareAmountPerKilometer;
+
+    if(driverVehicleType == "bike")
+    {
+      double resultFareAmount = (totalFareAmount.truncate()) / 2.0;
+      return resultFareAmount;
+    }
+    else if(driverVehicleType == "uber-go")
+    {
+      return totalFareAmount.truncate().toDouble();
+    }
+    else if(driverVehicleType == "uber-x")
+    {
+      double resultFareAmount = (totalFareAmount.truncate()) * 2.0;
+      return resultFareAmount;
+    }
+    else
+    {
+      return totalFareAmount.truncate().toDouble();
+    }
   }
 }
